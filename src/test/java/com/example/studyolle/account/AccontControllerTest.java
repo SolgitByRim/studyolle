01b366aea4b4ac40b09de7412a2b1bb61564d99b
kirim lee <ki@kirimui-MacBookPro.local>
2022-08-08 21:06:46 +0900

    회원가입 컨트롤러까지 진행

---
+++ src/test/java/com/example/studyolle/account/AccontControllerTest.java
@@ -0,0 +1,27 @@
+package com.example.studyolle.account;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class AccontControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @DisplayName("회원가입 화면 보이늬 ")
+    @Test
+    void signUpForm() throws Exception {
+        mockMvc.perform(get("/sign-up"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("account/sign-up"));
+    }
+}
